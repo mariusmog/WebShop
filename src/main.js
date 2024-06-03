affb8f174ff9eb88dfd6c68aa9bb4eb652f8c78b
mariusmog <marius.cahagne@gmail.com>
2024-06-03 15:18:54 +0200

    connexion base de données

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import './assets/main.css'
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from 'bootstrap-vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
